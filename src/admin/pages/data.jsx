47ede324890a6bdd0c104c49c75370677350a2ae
Alazar <almaxvsbeko@gmail.com>
2024-05-11 22:56:59 +0300

    css change

--- src/admin/pages/data.jsx
+++ src/admin/pages/data.jsx
@@ -145,7 +145,7 @@ const Data = () => {
                     <td className="px-6 py-4 whitespace-nowrap">
                       <span class="inline-flex items-center bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
                         <span class="w-2 h-2 me-1 bg-green-500 rounded-full"></span>
-                        Available
+                        Active
                       </span>
                     </td>
                   </tr>
